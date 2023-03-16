04716a1f056f3c6adcc567c976138ab5d7c762a1
Abdul Rafay <rafaymalick68@gmail.com>
2023-03-16 17:11:50 -0600

    thrusday

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -11,7 +11,7 @@ const NavigationDots = ({ active }) => {
                         key={item + index}
                         className="app__navigation-dot"
                         style={active === item ? { backgroundColor: '#CF0A0A' } : {}}
-                    />
+                    > </a>
 
                 )
             )}
--- src/containers/Work/Work.jsx
+++ src/containers/Work/Work.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { AppWrap } from '../../wrapper/index';
+import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
-  return (
-    <div>Work</div>
-  )
-}
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWorks, setFilterWorks] = useState([])
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWorks(data);
+      });
+  }, [])
 
-export default Work
+  const filterHandler = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWorks(works);
+      } else {
+        setFilterWorks(works.filter((work) => work.tags.includes(item)));
+      }
+    }, 500);
+  };
+  return (
+    <>
+      <h2 className='head-text'>
+        My <span>Portfolio</span>
+      </h2>
+      <div className='app__work-filter'>
+        {[
+          'WordPress',
+          'HTML5',
+          'JavaScript',
+          'CSS3',
+          'React Js',
+          'Next Js',
+          'All',
+        ].reverse().map((item, index) => (
+          <button
+            key={index}
+            onClick={() => filterHandler(item)}
+            className={`app__work-filter-item p-text ${activeFilter === item ? 'itemActive' : ''}`}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: .9, delayChildren: 0.9 }}
+        className="app__work-portfolio"
+      >
+        {filterWorks.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={urlFor(work.imgUrl)} alt={work.title} />
+              <h2>{work.title}</h2>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
+};
+
+export default AppWrap(Work, 'work');
