2a1ae893780328b4cfdcbf5b521959c9bcfe2d87
Abdul Rafay <rafaymalick68@gmail.com>
2023-04-19 03:02:14 +0500

    fixed mutation

--- src/client.js
+++ src/client.js
@@ -6,7 +6,8 @@ export const client = createClient({
     dataset: 'production',
     apiVersion: '2023-03-16',
     useCdn: true,
-    token: process.env.SANITY_PROJECT_KEY,
+    token: 'skhpGASuK5zAzGqmCGv9TnfTMYd3Gv8BnFXCVEehDR0jPcA8bYS8yhnSCzHpJCqzV6C2xXkJ2IoiE98nqpGceFFTCXLfAyLkDiL8LOStvdDcTkgm3fj36NkJiMMRwjEvVc8mZlyWs1YuJ5mf5A6jkcfLONSkCv71Ra8EDb7AcNEd5qEoNI3M',
+    // token: process.env.SANITY_PROJECT_KEY,
 })
 
 const builder = ImageUrlBuilder(client);
